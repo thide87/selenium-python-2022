8db890b765f5f4f8a73eb94bfcdf3ac455dc2056
Edith Guzman Rivera <thide87@hotmail.com>
2022-06-03 03:22:24 -0500

    pytest test cases

---
+++ Test Cases/test_example.py
@@ -0,0 +1,9 @@
+class TestSuite:
+        def test_frist(self):
+            pass
+        def test_second(self):
+            pass
+        def test_third(self):
+            pass
+        def do_something(self):
+            pass
---
+++ Test Cases/test_par.py
@@ -0,0 +1,15 @@
+def es_par(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def test_positive():
+    result = es_par(2, 4)
+    assert result
+
+
+def test_negative():
+    result = es_par(3, 9)
+    assert not result
